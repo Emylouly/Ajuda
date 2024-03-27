2d3e0ca925ab9c6e2d663e40782d8d2ed248cc49
Emylouly <emilylouisemika@gmail.com>
2024-03-27 14:22:13 -0300

    a

--- teste/Primeiro/src/Principal.java
+++ teste/Primeiro/src/Principal.java
@@ -1,68 +1,40 @@
-import java.util.Scanner;
-
 public class Principal {
 
     public static void main(String[] args) {
         
-        Scanner input = new Scanner(System.in);
-        CadastroConta conta = new CadastroConta();
-        int escolha;
-        String nome, valor;
+        int tam=10, aux;
+        int n[] = new int[tam];
 
-        for(int i =1; i!=0; i++){
+        n[1] = 1;
+        n[2] = 10;
+        n[3] = 4;
+        n[4] = 3;
+        n[5] = 2;
+        n[6] = 9;
+        n[7] = 7;
+        n[8] = 11;
 
-            System.out.println("Digite a sua escolha: ");
-            System.out.println("1 - para cadastrar: ");
-            System.out.println("2 - para listar: ");
-            System.out.println("3 - para procurar: ");
-            System.out.println("4 - para sair: ");
-            System.out.println("\n\nInsira aqui: ");
+        for(int i =0; i<tam; i++){
 
-            escolha = input.nextInt();
+            if(n[i]>n[i-1]){
 
-            switch (escolha) {
-                case 1:
-                    
-                    System.out.println("\n\nDigite o nome: ");
-                    nome = input.next();
-                    System.out.println("Digite o valor: ");
-                    valor = input.next();
+                aux = n[i];
+                n[i] = n[i-1];
+                n[i-1] = aux;
 
-                    conta.cadastrar(nome, valor);
-
-                    break;
-
-                case 2:
-
-                    conta.listar();
-
-                    break;
-                
-                case 3:
-
-                    System.out.println("Digite o nome do usuário da conta: ");
-                    nome = input.next();
-                    conta.pesquisa(nome);
-
-                    break;
-                
-                case 4:
-
-                    System.out.println("Saindo!!");
-                    System.out.println(0);
-
-                    break;
-            
-                default:
-
-                    System.out.println("Escola inválida!!!");
-
-                    break;
             }
 
         }
 
+        for(int i=0; i<tam; i++){
+
+            System.out.print(n[i] + "  ");
+
+        }
+
+    
 
     }
+    
 
 }
