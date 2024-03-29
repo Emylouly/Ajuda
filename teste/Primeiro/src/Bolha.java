1a99273488417672759142f445bf2c7c3dc37312
Emylouly <emilylouisemika@gmail.com>
2024-03-28 22:52:26 -0300

    Update Bolha.java

--- teste/Primeiro/src/Bolha.java
+++ teste/Primeiro/src/Bolha.java
@@ -1,19 +1,21 @@
 public class Bolha {
     public static void main(String[] args) {
 
-        int tam=7, aux;
-        int [] n = {5, 1, 12, -5, 16, 0, 100};
+        int tam=5, aux;
+        int [] n = {5, 1, 12, -5, 16};
 
         for(int i = 0; i<tam; i++){
             for(int x =0; x<tam-1; x++){
+                System.out.println(x);
 
                 if(n[x]>n[x+1]){
+
 
                 aux = n[x];
                 n[x] = n[x+1];
                 n[x+1] = aux;
 
-                }      
+                }
             }
         }
 
